09c8e331e0c9e4c100594244b6b65a48c838af7a
gameP147 <81524342+gameP147@users.noreply.github.com>
2021-03-29 13:04:30 +0300

    shyfff

---
+++ Assets/kk.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class kk : MonoBehaviour
+{
+    public GameObject playerPrefab;
+    public void Start()
+    {
+        if (playerPrefab == null)
+        {
+            Debug.LogError("<Color=Red><a>Missing</a></Color> playerPrefab Reference. Please set it up in GameObject 'Game Manager'", this);
+        }
+        else
+        {
+            Debug.LogFormat("We are Instantiating LocalPlayer from {0}", Application.loadedLevelName);
+            // we're in a room. spawn a character for the local player. it gets synced by using PhotonNetwork.Instantiate
+            PhotonNetwork.Instantiate(this.playerPrefab.name, new Vector3(0f, 5f, 0f), Quaternion.identity, 0);
+        }
+
+    }
+
+
+}    
+    // Start is called before the first frame updae
+    
+
---
+++ Assets/klk.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Photon.Pun;
+
+public class klk : MonoBehaviour
+{
+    public static GameObject LocalPlayerInstance;
+    public PhotonView pw;
+
+    void Awake()
+    {
+        // #Important
+        // used in GameManager.cs: we keep track of the localPlayer instance to prevent instantiation when levels are synchronized
+        if (pw.IsMine)
+        {
+            klk.LocalPlayerInstance = this.gameObject;
+        }
+        // #Critical
+        // we flag as don't destroy on load so that instance survives level synchronization, thus giving a seamless experience when levels load.
+        DontDestroyOnLoad(this.gameObject);
+
+    }
+    void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.W))
+        {
+            transform.Translate(new Vector3(0, 0, 5) * Time.deltaTime);
+        }
+        
+        
+    }
+
+}
